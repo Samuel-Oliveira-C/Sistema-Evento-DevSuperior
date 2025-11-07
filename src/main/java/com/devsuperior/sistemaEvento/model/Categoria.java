01c61f2d94a14c449c9d3209e8410e699fdff812
Samuel Oliveira <samuel.oleiv@gmail.com>
2025-11-07 14:40:08 -0300

    Foi criado a entidade Categoria e Adicionado a dependencia para criação de ULID

---
+++ src/main/java/com/devsuperior/sistemaEvento/model/Categoria.java
@@ -0,0 +1,80 @@
+package com.devsuperior.sistemaEvento.model;
+
+
+
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    @Id
+    private Ulid id = UlidCreator.getUlid();
+
+    @Column(columnDefinition ="TEXT")
+    private String descricao;
+
+    public Categoria() {}
+
+    public Categoria(Ulid id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+
+    public Ulid getId() {
+        return id;
+    }
+    public void setId(Ulid id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Categoria [id=" + getId() + ", descricao=" + getDescricao() + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+}
