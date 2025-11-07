7e7f9714babbad334d41ebd94947726467a3cb07
Samuel Oliveira <samuel.oleiv@gmail.com>
2025-11-07 16:10:12 -0300

    Foi criado o seeding para atividade e foi adicionado o @JoinColumn na atividade

--- src/main/java/com/devsuperior/sistemaEvento/model/Atividade.java
+++ src/main/java/com/devsuperior/sistemaEvento/model/Atividade.java
@@ -5,6 +5,7 @@ import com.github.f4b6a3.ulid.Ulid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -21,7 +22,8 @@ public class Atividade {
 
     private Double preco;
 
-    @ManyToOne
+    @ManyToOne()
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Atividade() {}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,10 @@
 INSERT INTO tb_categoria (id, descricao) VALUES ('01HZYZKJ7X6YV4M9JQ2ZK3X4W7', 'Tecnologia');
 
+INSERT INTO tb_atividade (id, nome, descricao, preco) VALUES ('01EX8Y21KBH49ZZCA7KSKH6X1C', 'Curso de Java', 'Curso completo de Java com Spring Boot', 499.90);
+
+
+
+
 -- Aqui eu adicionei diretamente um ID ULID, por que para a atividade não é 
 -- necessário implementar o repository.
 
